2b667bca4e5c3aa22154313e2fcf852b4d73c60a
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-13 19:06:35 -0300

    Impl Navbar Desktop

--- FrontendNotas/src/components/Navbar.jsx
+++ FrontendNotas/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <header>
-            <h2>navbar</h2>
-        </header>
+        <>
+            <header>
+                <nav>
+                    <Link to={"/Login"}>Login</Link>
+                    <Link to={"/Register"}>Register</Link>
+                </nav>
+                <div className="imgLogo">
+                    <Link to={"/Home"}><img src="/favicon.ico" /></Link>
+                </div>
+            </header>
+        </>
     )
 }
 
