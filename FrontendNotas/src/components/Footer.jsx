1e934c449ab13867ac301f02b83f651ac8d3ac1a
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-13 19:11:28 -0300

    Impl Footer

--- FrontendNotas/src/components/Footer.jsx
+++ FrontendNotas/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
     return (
         <footer>
-            <h2>Footer</h2>
+            <h2>Made with ❤️ in Argentina by <Link to={"https://tomas-alberdi.com"} target='_blank'>Tomás Alberdi</Link></h2>
         </footer>
     )
 }
