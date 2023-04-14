78bc2f08a6a1c831be3af89423c5b65a34927497
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-13 23:24:13 -0300

    Started register form

--- FrontendNotas/src/components/pages/Register.jsx
+++ FrontendNotas/src/components/pages/Register.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Register = () => {
+
+    const errorText = (texto) => {
+        let error = document.createElement('span')
+        error.classList.add('error')
+        error.innerText = texto
+        return error
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const username = e.target.username
+        const email = e.target.email
+        const password = e.target.password
+        const passwordConfirm = e.target.passwordConfirm
+        document.querySelectorAll('.error').forEach(e => {
+            e.remove()
+        })
+        document.querySelectorAll('input').forEach(e => {
+            e.classList.remove("invalid")
+        })
+        const validate = () => {
+            if ((!username.value)||(/^\S{6,20}$/.test(username.value) === false)) {
+                let error = errorText("The username should have between 6 and 20 characters")
+                username.after(error)
+                username.classList.add("invalid")
+                return false
+            }
+            if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value) === false) {
+                let error = errorText("Invalid email")
+                email.after(error)
+                email.classList.add("invalid")
+                return false
+            }
+            if (/^.{7,20}$/.test(password.value) === false) {
+                let error = errorText("The password should have between 7 and 20 characters")
+                password.after(error)
+                password.classList.add("invalid")
+                return false
+            }
+            if (passwordConfirm.value !== password.value) {
+                passwordConfirm.classList.add("invalid")
+                return false
+            }
+            return true
+        }
+        if (validate()) {
+            console.log("yes")
+        } else {
+            console.log("no")
+        }
+    }
+
     return (
         <section className="Register">
-            <h2>Register</h2>
+            <h1>Create Account</h1>
+            <form onSubmit={onSubmit}>
+                <section>
+                    <input name='username' placeholder='Username' />
+                </section>
+                <section>
+                    <input name='email' placeholder='Email' />
+                </section>
+                <section>
+                    <input name='password' placeholder='Password' />
+                </section>
+                <section>
+                    <input name='passwordConfirm' placeholder='Confirm Password' />
+                </section>
+                <button>Create Account</button>
+            </form>
+            <h3>Already have an account? <Link to={"/Login"}>Log In</Link></h3>
         </section>
     )
 }
