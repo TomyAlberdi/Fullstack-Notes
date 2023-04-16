06ac704e1546cdc29a766e90ad6aa142ac280d8b
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-16 18:54:21 -0300

    Implemented add note functionality

--- FrontendNotas/src/components/pages/Home.jsx
+++ FrontendNotas/src/components/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Note from '../Note'
 import Masonry from 'react-masonry-css'
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
 
 const Home = ({User,Token}) => {
 
@@ -35,12 +37,53 @@ const Home = ({User,Token}) => {
         1023: 3,
         765: 2,
     }
+
+    const submitNote = async (e) => {
+        e.preventDefault()
+        const title = e.target.noteTitle
+        const text = e.target.noteText
+        const url = "http://192.168.0.183:8080/notes/add"
+        const aux = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${Token}`
+            },
+            body: JSON.stringify({
+                "title":title.value,
+                "text":text.value,
+                "user_id":User.id
+            })
+        }
+        try {
+            const res = await fetch(url,aux)
+            if (res.ok) {
+                setLoadingNotes(true)
+                title.value = "",
+                text.value = ""
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'An error occurred while adding a note.',
+                    text: 'Please try again in a few minutes'
+                })
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
     
     return (
         <section className="Home">
             <h1>My Notes</h1>
             <section className="newNote">
-                <h2>New note</h2>
+                <form onSubmit={submitNote}>
+                    <div className="newNoteInputs">
+                        <input type="text" name="noteTitle" id="noteTitle" placeholder='Title' required />
+                        <input type="text" name="noteText" id="noteText" placeholder='Add a note...' />
+                    </div>
+                    <button type="submit"><i className='fa-solid fa-plus'></i></button>
+                </form>
             </section>
             <section className="notes">
                 {
