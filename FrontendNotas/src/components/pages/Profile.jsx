654bb51beff0954222bad6db216a3f3dfed3628b
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2023-05-15 16:36:45 -0300

    Implemented profile page frontend

--- FrontendNotas/src/components/pages/Profile.jsx
+++ FrontendNotas/src/components/pages/Profile.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Profile = () => {
+const Profile = ({User}) => {
   return (
     <section className="Profile">
-      <h1>Profile</h1>
+      <section className="profileInfo">
+        <div className="div1">
+          <section>
+            {User.username.charAt(0).toUpperCase()}
+          </section>
+        </div>
+        <div className="div2">
+          {User.username}
+        </div>
+        <div className="div3">
+          {User.email}
+        </div>
+        <div className="div4">
+          description
+        </div>
+        <Link className="div5">
+          Edit Profile <i className="fa-solid fa-pen-to-square"></i>
+        </Link>
+      </section>
     </section>
   )
 }
